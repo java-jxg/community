9bc5db36244d5c34590f886d131adb9d1f6620cb
java-jxg <java_jxg@163.com>
2019-12-07 08:37:48 +0800

    extract github config form code to properties

---
+++ src/main/java/com/fastcase/community/community/controller/AuthorizeController.java
@@ -0,0 +1,43 @@
+package com.fastcase.community.community.controller;
+
+import com.fastcase.community.community.dto.AccessTokenDTO;
+import com.fastcase.community.community.dto.GithubUser;
+import com.fastcase.community.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    public String clientId;
+
+    @Value("${github.client.secret}")
+    public String clientSecret;
+
+    @Value("${github.redirect.url}")
+    public String clientUrl;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_url(clientUrl);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        System.out.println(accessToken);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        System.out.println(user.getId());
+        System.out.println("新修版");
+        return "index";
+    }
+}
--- src/main/java/com/fastcase/community/community/controller/IndexController.java
+++ src/main/java/com/fastcase/community/community/controller/IndexController.java
@@ -3,16 +3,14 @@ package com.fastcase.community.community.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
+public class IndexController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name") String name, Model model){
-        model.addAttribute("name",name);
-    return "hello";
+    @GetMapping("/")
+    public String hello(){
+    return "index";
 
     }
 
