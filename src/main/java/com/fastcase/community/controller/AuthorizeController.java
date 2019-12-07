7bf77992454cf5e391812dafabea6f7b6260faad
java-jxg <java_jxg@163.com>
2019-12-07 13:07:26 +0800

    add mysql

--- src/main/java/com/fastcase/community/controller/AuthorizeController.java
+++ src/main/java/com/fastcase/community/controller/AuthorizeController.java
@@ -2,12 +2,17 @@ package com.fastcase.community.controller;
 
 import com.fastcase.community.dto.AccessTokenDTO;
 import com.fastcase.community.dto.GithubUser;
+import com.fastcase.community.mapper.UserMapper;
+import com.fastcase.community.model.User;
 import com.fastcase.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -22,10 +27,13 @@ public class AuthorizeController {
 
     @Value("${github.redirect.url}")
     public String clientUrl;
+    @Autowired
+    private UserMapper userMapper;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -33,11 +41,20 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_url(clientUrl);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        System.out.println(accessToken);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        System.out.println(user.getId());
-        System.out.println("新修版");
-        return "index";
+        GithubUser githubUseruser = githubProvider.getUser(accessToken);
+        if(githubUseruser != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUseruser.getName());
+            user.setAccountId(String.valueOf(githubUseruser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+
+            request.getSession().setAttribute("user",githubUseruser);
+            return "redirect:/";
+        }else{
+            return "redirect:/";
+        }
     }
 }
