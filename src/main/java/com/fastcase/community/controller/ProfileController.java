1e86bdcf2c4b8109845abc95d5e48419251c0ee6
java-jxg <java_jxg@163.com>
2019-12-11 09:41:09 +0800

    添加拦截器

--- src/main/java/com/fastcase/community/controller/ProfileController.java
+++ src/main/java/com/fastcase/community/controller/ProfileController.java
@@ -1,6 +1,5 @@
 package com.fastcase.community.controller;
 
-import com.fastcase.community.mapper.UserMapper;
 import com.fastcase.community.model.User;
 import com.fastcase.community.service.QuestionService;
 import com.github.pagehelper.PageInfo;
@@ -11,13 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ProfileController {
-    @Autowired
-    private UserMapper userMapper;
     @Autowired
     private QuestionService questionService;
 
@@ -26,26 +22,12 @@ public class ProfileController {
                           @PathVariable(name = "action") String action,
                           Model model,
                           @RequestParam(name = "page", defaultValue = "1") Integer page,
-                          @RequestParam(name = "size", defaultValue = "2") Integer size) {
-        User user = null;
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
+                          @RequestParam(name = "size", defaultValue = "10") Integer size) {
 
+        User user = (User) request.getSession().getAttribute("user");
         if (user == null) {
             return "redirect:/";
         }
-
 
         if ("questions".equals(action)) {
             model.addAttribute("section", "questions");
